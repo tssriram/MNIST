805f8eeac8488c42fe55b86bd506c4ab722c893c
tssriram <tssriram@yahoo.com>
2020-05-10 00:05:29 +0530

    Update mnistmlpe1.py

--- mnistmlpe1.py
+++ mnistmlpe1.py
@@ -10,47 +10,50 @@ from sklearn.neural_network import MLPClassifier
 
 # In[2]:
 
-
+# Dataset from Kaggle MNIST
 train=pd.read_csv("C:/Users/Tarun/Desktop/New folder/train.csv")
 
 
 # In[3]:
 
-
+# Peruse the data
 train.head()
 
 
 # In[4]:
 
-
+# Standardizing the pixel intensity values from 0-1
 train.iloc[:,1:785]=train.iloc[:,1:785]/255
 
 
 # In[5]:
 
 
-
+# Defining the classifier object
 mlp = MLPClassifier(hidden_layer_sizes=(28,28,28,28),activation='relu',solver='sgd')
 # With this setup accuracy tested on Kaggle was 0.95514
 
 #mlp = MLPClassifier(hidden_layer_sizes=(28,28,28,28),activation='relu',solver='sgd',max_iter=1000)
 # With this setup accuracy tested on Kaggle was 0.954
 
+#mlp = MLPClassifier(hidden_layer_sizes=(28,28,28,28),activation='relu',solver='lbfgs',max_iter=400)
+# With this setup accuracy tested on Kaggle was 0.95214
+
 # In[6]:
 
-
+# Training the model
 mlp.fit(train.iloc[:,1:785],train.iloc[:,0])
 
 
 # In[7]:
 
-
+#Reading in the test dataset from Kaggle MNIST
 test=pd.read_csv("C:/Users/Tarun/Desktop/New folder/test.csv")
 
 
 # In[8]:
 
-
+# Standardizing it for making predictions
 test=test/255
 
 
@@ -74,7 +77,7 @@ print(pred)
 
 # In[19]:
 
-
+# Regular format for submission
 a=pd.Series(range(0,28000))
 predi={'ImageId':a,'Label':pred}
 final=pd.DataFrame(predi)
@@ -88,7 +91,7 @@ print(final)
 
 # In[22]:
 
-
+#Output into a file
 final.to_csv('finalmnistmlp.csv', index='False')
 
 
